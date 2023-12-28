ceadeff476b9c9d8696e8293e3239b0e59ec637f
BelliniGauss <118434152+BelliniGauss@users.noreply.github.com>
2023-12-28 16:10:32 +0100

    Update fileToFux.bat

--- fileToFux.bat
+++ fileToFux.bat
@@ -1,9 +1,10 @@
 @echo se vedi questo si è eseguito uno script malevolo che ha scaricato file sul pc indisturbato
 @echo potrebbe tranquillamente rubarti tutte le password...
 pause
-pause
-pause
-@echo emilino entry point in C:\Users\Public\test.bat
+@echo
+@echo
+@echo Elimino entry point in   C:\Users\Public\test.bat
+@echo
+@echo
 del C:\Users\Public\test.bat 
-pause
 pause
